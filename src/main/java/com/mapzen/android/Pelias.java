3553413296ebcd987f760912fd7020b27db1d82a
baldur <baldur.gudbjornsson@gmail.com>
2014-04-03 10:21:41 -0400

    making Pelias a singleton

--- src/main/java/com/mapzen/android/Pelias.java
+++ src/main/java/com/mapzen/android/Pelias.java
@@ -7,17 +7,25 @@ import retrofit.RestAdapter;
 
 public class Pelias {
     PeliasService service;
-    String endpoint = "http://pelias.test.mapzen.com/";
+    static String endpoint = "http://pelias.test.mapzen.com/";
+    static Pelias instance = null;
 
     protected Pelias(PeliasService service) {
         this.service = service;
     }
 
-    public Pelias() {
+    private Pelias() {
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint(endpoint)
                 .build();
         this.service = restAdapter.create(PeliasService.class);
+    }
+
+    public static Pelias getPelias() {
+        if (instance == null) {
+            instance = new Pelias();
+        }
+        return instance;
     }
 
     public void suggest(String query, Callback<Result> callback) {
