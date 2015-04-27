3fae3623ccbf6585eb8a39a1af5398826cdef452
Chuck Greb <charles.greb@gmail.com>
2015-04-27 15:25:46 -0400

    Adds animation for show/hide list view and IME.

--- src/main/java/com/mapzen/pelias/widget/PeliasSearchView.java
+++ src/main/java/com/mapzen/pelias/widget/PeliasSearchView.java
@@ -1,26 +1,116 @@
 package com.mapzen.pelias.widget;
 
+import com.mapzen.pelias.R;
+
 import android.content.Context;
+import android.os.ResultReceiver;
 import android.support.v7.widget.SearchView;
-import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ListView;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import static android.view.animation.AnimationUtils.loadAnimation;
+
 public class PeliasSearchView extends SearchView {
+    public static final String TAG = PeliasSearchView.class.getSimpleName();
+
+    private static final AutoCompleteTextViewReflector HIDDEN_METHOD_INVOKER =
+            new AutoCompleteTextViewReflector();
+
+    private Runnable showImeRunnable = new Runnable() {
+        public void run() {
+            final InputMethodManager imm = (InputMethodManager)
+                    getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                HIDDEN_METHOD_INVOKER.showSoftInputUnchecked(imm, PeliasSearchView.this, 0);
+            }
+        }
+    };
+
+    private Runnable hideImeRunnable = new Runnable() {
+        public void run() {
+            final InputMethodManager imm = (InputMethodManager)
+                    getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                imm.hideSoftInputFromWindow(getWindowToken(), 0);
+            }
+        }
+    };
+
     public PeliasSearchView(Context context) {
         super(context);
-    }
-
-    public PeliasSearchView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        disableDefaultSoftKeyboardBehaviour();
     }
 
     public void setAutoCompleteListView(final ListView listView) {
         setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override public void onFocusChange(View view, boolean hasFocus) {
-                final int visibility = hasFocus ? VISIBLE : GONE;
-                listView.setVisibility(visibility);
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    final Animation slideIn = loadAnimation(getContext(), R.anim.slide_in);
+                    listView.setAnimation(slideIn);
+                    listView.setVisibility(VISIBLE);
+                    postDelayed(showImeRunnable, 300);
+                } else {
+                    final Animation slideOut = loadAnimation(getContext(), R.anim.slide_out);
+                    listView.setAnimation(slideOut);
+                    listView.setVisibility(GONE);
+                    postDelayed(hideImeRunnable, 300);
+                }
             }
         });
+    }
+
+    /**
+     * Overrides default behavior for showing soft keyboard. Enables manual control by this class.
+     */
+    private void disableDefaultSoftKeyboardBehaviour() {
+        try {
+            Field showImeRunnable = SearchView.class.getDeclaredField("mShowImeRunnable");
+            showImeRunnable.setAccessible(true);
+            showImeRunnable.set(this, new Runnable() {
+                @Override
+                public void run() {
+                    // Do nothing.
+                }
+            });
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            Log.e(TAG, "Unable to override default soft keyboard behavior", e);
+        }
+    }
+
+    /**
+     * Copied from {@link android.support.v7.widget.SearchView.AutoCompleteTextViewReflector}.
+     */
+    private static class AutoCompleteTextViewReflector {
+        private Method showSoftInputUnchecked;
+
+        AutoCompleteTextViewReflector() {
+            try {
+                showSoftInputUnchecked = InputMethodManager.class.getMethod(
+                        "showSoftInputUnchecked", int.class, ResultReceiver.class);
+                showSoftInputUnchecked.setAccessible(true);
+            } catch (NoSuchMethodException e) {
+                // Ah well.
+            }
+        }
+
+        void showSoftInputUnchecked(InputMethodManager imm, View view, int flags) {
+            if (showSoftInputUnchecked != null) {
+                try {
+                    showSoftInputUnchecked.invoke(imm, flags, null);
+                    return;
+                } catch (Exception e) {
+                }
+            }
+
+            // Hidden method failed, call public version instead
+            imm.showSoftInput(view, flags);
+        }
     }
 }
--- src/test/java/com/mapzen/pelias/widget/PeliasSearchViewTest.java
+++ src/test/java/com/mapzen/pelias/widget/PeliasSearchViewTest.java
@@ -1,13 +1,13 @@
 package com.mapzen.pelias.widget;
 
 import com.mapzen.pelias.BuildConfig;
+import com.mapzen.pelias.ShadowInputMethodManager;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 
 import android.app.Activity;
@@ -17,9 +17,10 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, emulateSdk = 21)
+@Config(constants = BuildConfig.class, emulateSdk = 21, shadows = {ShadowInputMethodManager.class})
 public class PeliasSearchViewTest {
     private static final Activity ACTIVITY = Robolectric.setupActivity(Activity.class);
 
@@ -41,7 +42,7 @@ public class PeliasSearchViewTest {
         listView.setVisibility(View.GONE);
         peliasSearchView.setAutoCompleteListView(listView);
         AutoCompleteTextView queryText = getQueryTextView();
-        Shadows.shadowOf(queryText).setViewFocus(true);
+        shadowOf(queryText).setViewFocus(true);
         assertThat(listView.getVisibility()).isEqualTo(View.VISIBLE);
     }
 
@@ -51,8 +52,32 @@ public class PeliasSearchViewTest {
         listView.setVisibility(View.VISIBLE);
         peliasSearchView.setAutoCompleteListView(listView);
         AutoCompleteTextView queryText = getQueryTextView();
-        Shadows.shadowOf(queryText).setViewFocus(false);
+        shadowOf(queryText).setViewFocus(false);
         assertThat(listView.getVisibility()).isEqualTo(View.GONE);
+    }
+
+    @Test
+    public void setAutoCompleteListView_shouldShowImeWhenQueryGetsFocus() throws Exception {
+        ListView listView = new ListView(ACTIVITY);
+        listView.setVisibility(View.GONE);
+        peliasSearchView.setAutoCompleteListView(listView);
+        AutoCompleteTextView queryText = getQueryTextView();
+        shadowOf(queryText).setViewFocus(false);
+        shadowOf(queryText).setViewFocus(true);
+        Robolectric.flushForegroundScheduler();
+        assertThat(ShadowInputMethodManager.isSoftInputVisible()).isTrue();
+    }
+
+    @Test
+    public void setAutoCompleteListView_shouldHideImeWhenQueryGetsFocus() throws Exception {
+        ListView listView = new ListView(ACTIVITY);
+        listView.setVisibility(View.GONE);
+        peliasSearchView.setAutoCompleteListView(listView);
+        AutoCompleteTextView queryText = getQueryTextView();
+        shadowOf(queryText).setViewFocus(true);
+        shadowOf(queryText).setViewFocus(false);
+        Robolectric.flushForegroundScheduler();
+        assertThat(ShadowInputMethodManager.isSoftInputVisible()).isFalse();
     }
 
     private AutoCompleteTextView getQueryTextView() {
