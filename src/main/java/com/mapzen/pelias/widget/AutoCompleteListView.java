9f0586000e3cb45e324888508e8694677e964a17
Chuck Greb <charles.greb@gmail.com>
2015-04-28 11:31:04 -0400

    Adds autocomplete list view

---
+++ src/main/java/com/mapzen/pelias/widget/AutoCompleteListView.java
@@ -0,0 +1,93 @@
+package com.mapzen.pelias.widget;
+
+import com.mapzen.pelias.R;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class AutoCompleteListView extends ListView {
+    private View headerView;
+    private View emptyView;
+
+    public AutoCompleteListView(Context context) {
+        super(context);
+        init();
+    }
+
+    public AutoCompleteListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public AutoCompleteListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        headerView = inflate(getContext(), R.layout.recent_search_list_header, null);
+        addHeaderView(headerView);
+        setHeaderDividersEnabled(false);
+    }
+
+    public void showHeader() {
+        headerView.findViewById(R.id.recent_search_header).setVisibility(VISIBLE);
+    }
+
+    public void hideHeader() {
+        headerView.findViewById(R.id.recent_search_header).setVisibility(GONE);
+    }
+
+    public boolean isHeaderVisible() {
+        return headerView.findViewById(R.id.recent_search_header).getVisibility() == VISIBLE;
+    }
+
+    @Override
+    public void setEmptyView(View emptyView) {
+        this.emptyView = emptyView;
+        updateEmptyView();
+    }
+
+    @Override
+    public View getEmptyView() {
+        return emptyView;
+    }
+
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(visibility);
+        updateEmptyView();
+    }
+
+    @Override
+    public void setAdapter(final ListAdapter adapter) {
+        super.setAdapter(adapter);
+        adapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                updateEmptyView();
+            }
+        });
+    }
+
+    private void updateEmptyView() {
+        if (emptyView == null) {
+            return;
+        }
+
+        if (getVisibility() != VISIBLE) {
+            emptyView.setVisibility(View.GONE);
+            return;
+        }
+
+        if (getAdapter() == null || getAdapter().isEmpty()) {
+            emptyView.setVisibility(VISIBLE);
+        } else {
+            emptyView.setVisibility(GONE);
+        }
+    }
+}
---
+++ src/test/java/com/mapzen/pelias/widget/AutoCompleteListViewTest.java
@@ -0,0 +1,143 @@
+package com.mapzen.pelias.widget;
+
+import com.mapzen.pelias.BuildConfig;
+import com.mapzen.pelias.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.robolectric.Robolectric.buildActivity;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 21)
+public class AutoCompleteListViewTest {
+    private static final Activity ACTIVITY = buildActivity(Activity.class).create().get();
+
+    private AutoCompleteListView autoCompleteListView;
+
+    @Before
+    public void setUp() throws Exception {
+        autoCompleteListView = new AutoCompleteListView(ACTIVITY);
+    }
+
+    @Test
+    public void shouldNotBeNull() throws Exception {
+        assertThat(autoCompleteListView).isNotNull();
+    }
+
+    @Test
+    public void shouldHaveHeaderView() throws Exception {
+        assertThat(autoCompleteListView.getHeaderViewsCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void showHeader_shouldAddHeaderView() throws Exception {
+        autoCompleteListView.hideHeader();
+        autoCompleteListView.showHeader();
+        assertThat(autoCompleteListView.findViewById(R.id.recent_search_header).getVisibility())
+                .isEqualTo(View.VISIBLE);
+    }
+
+    @Test
+    public void hideHeader_shouldRemoveHeaderView() throws Exception {
+        autoCompleteListView.showHeader();
+        autoCompleteListView.hideHeader();
+        assertThat(autoCompleteListView.findViewById(R.id.recent_search_header).getVisibility())
+                .isEqualTo(View.GONE);
+    }
+
+    @Test
+    public void isHeaderVisible_shouldReportHeaderVisibility() throws Exception {
+        autoCompleteListView.showHeader();
+        assertThat(autoCompleteListView.isHeaderVisible()).isTrue();
+
+        autoCompleteListView.hideHeader();
+        assertThat(autoCompleteListView.isHeaderVisible()).isFalse();
+    }
+
+    @Test
+    public void setAdapter_shouldHideEmptyViewWhenItemIsAddedToAdapter() throws Exception {
+        ArrayAdapter adapter = new ArrayAdapter(ACTIVITY, 0);
+        View empty = new View(ACTIVITY);
+        autoCompleteListView.setEmptyView(empty);
+        autoCompleteListView.setAdapter(adapter);
+        autoCompleteListView.setVisibility(View.VISIBLE);
+        adapter.add(new Object());
+        assertThat(empty.getVisibility()).isEqualTo(View.GONE);
+    }
+
+    @Test
+    public void setAdapter_shouldShowEmptyViewWhenItemIsRemovedFromAdapter() throws Exception {
+        ArrayAdapter adapter = new ArrayAdapter(ACTIVITY, 0);
+        adapter.add(new Object());
+        View empty = new View(ACTIVITY);
+        autoCompleteListView.setEmptyView(empty);
+        autoCompleteListView.setAdapter(adapter);
+        autoCompleteListView.setVisibility(View.VISIBLE);
+        adapter.clear();
+        assertThat(empty.getVisibility()).isEqualTo(View.VISIBLE);
+    }
+
+    @Test
+    public void setAdapter_shouldHideEmptyViewWhenItemIsRemovedIfListViewHidden() throws Exception {
+        ArrayAdapter adapter = new ArrayAdapter(ACTIVITY, 0);
+        adapter.add(new Object());
+        View empty = new View(ACTIVITY);
+        autoCompleteListView.setEmptyView(empty);
+        autoCompleteListView.setAdapter(adapter);
+        autoCompleteListView.setVisibility(View.GONE);
+        adapter.clear();
+        assertThat(empty.getVisibility()).isEqualTo(View.GONE);
+    }
+
+    @Test
+    public void setVisibility_GONE_shouldHideEmptyView() throws Exception {
+        View empty = new View(ACTIVITY);
+        empty.setVisibility(View.VISIBLE);
+        autoCompleteListView.setEmptyView(empty);
+        autoCompleteListView.setVisibility(View.GONE);
+        assertThat(empty.getVisibility()).isEqualTo(View.GONE);
+    }
+
+    @Test
+    public void setVisibility_INVISIBLE_shouldHideEmptyView() throws Exception {
+        View empty = new View(ACTIVITY);
+        empty.setVisibility(View.VISIBLE);
+        autoCompleteListView.setEmptyView(empty);
+        autoCompleteListView.setVisibility(View.INVISIBLE);
+        assertThat(empty.getVisibility()).isEqualTo(View.GONE);
+    }
+
+    @Test
+    public void setVisibility_VISIBLE_shouldShowEmptyViewIfAdapterIsEmpty() throws Exception {
+        ArrayAdapter adapter = new ArrayAdapter(ACTIVITY, 0);
+        autoCompleteListView.setAdapter(adapter);
+
+        View empty = new View(ACTIVITY);
+        empty.setVisibility(View.GONE);
+        autoCompleteListView.setEmptyView(empty);
+        autoCompleteListView.setVisibility(View.VISIBLE);
+        assertThat(empty.getVisibility()).isEqualTo(View.VISIBLE);
+    }
+
+    @Test
+    public void setVisibility_VISIBILE_shouldHideEmptyViewIfAdapterIsNotEmpty() throws Exception {
+        ArrayAdapter adapter = new ArrayAdapter(ACTIVITY, 0);
+        adapter.add(new Object());
+        autoCompleteListView.setAdapter(adapter);
+
+        View empty = new View(ACTIVITY);
+        empty.setVisibility(View.VISIBLE);
+        autoCompleteListView.setEmptyView(empty);
+        autoCompleteListView.setVisibility(View.VISIBLE);
+        assertThat(empty.getVisibility()).isEqualTo(View.GONE);
+    }
+}
