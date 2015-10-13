e1d8b41686ef9ca6f19e00701b9325253342d44a
kingofirony <kingofirony@gmail.com>
2015-10-13 12:35:34 -0400

    Uses viewport instead of bounding box

--- src/main/java/com/mapzen/pelias/BoundingBox.java
+++ src/main/java/com/mapzen/pelias/BoundingBox.java
@@ -7,9 +7,16 @@ public class BoundingBox {
     public String maxLon;
 
     public BoundingBox(double minLat, double minLon, double maxLat, double maxLon) {
-        this.minLat = Double.toString(minLat);
-        this.minLon = Double.toString(minLon);
-        this.maxLat = Double.toString(maxLat);
-        this.maxLon = Double.toString(maxLon);
+        if(((maxLat - minLat) > 0.0) && ((maxLon - minLon) > 0.0)) {
+            this.minLat = Double.toString(minLat);
+            this.minLon = Double.toString(minLon);
+            this.maxLat = Double.toString(maxLat);
+            this.maxLon = Double.toString(maxLon);
+        } else {
+            this.minLat = Double.toString(maxLat);
+            this.minLon = Double.toString(maxLon);
+            this.maxLat = Double.toString(minLat);
+            this.maxLon = Double.toString(minLon);
+        }
     }
 }
